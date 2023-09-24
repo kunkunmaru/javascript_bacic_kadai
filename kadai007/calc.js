9dda78f6c712faf0f00eefc9e06be58f05860036
kunkunmaru <145129628+kunkunmaru@users.noreply.github.com>
2023-09-24 17:22:24 +0900

    kauai_007

---
+++ kadai007/calc.js
@@ -0,0 +1,17 @@
+//変数numをランダム数値にしてみる
+let num=Math.floor(Math.random() * 20);
+
+//ランダムにしてみたので確認のために数値を表示
+console.log(num);
+
+if (num % 3 ===0 && num % 5 ===0) {
+  console.log("3と5の倍数です");
+}
+
+else if (num % 3 === 0) {
+  console.log("3の倍数です");
+}
+
+else if (num % 5 ===0) {
+  console.log("5の倍数です");
+}
